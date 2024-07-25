304c58f39a220b4beae89e3258941b270ea65071
WY_Liu <liu.wangyang@outlook.com>
2024-07-25 10:46:28 +0800

    c

--- Assets/Scripts/SimpleMovementController.cs
+++ Assets/Scripts/SimpleMovementController.cs
@@ -4,65 +4,111 @@ using UnityEngine;
 
 public class SimpleMovementController : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 10f;
     public Transform Obj;
+    public Transform Obj2;
 
+    public float rotateSpeed = 60f;
+    public float rightSpeed = 70f;
+    private bool left = false;
+    private bool right = false;
+    private float rollValue = 0;
 
-    private float rotateSpeed = 30f;
-    private float movespeed = 5;
-    private bool left = false; 
-    private bool right = false; 
+    private Vector3 T = new Vector3(0f, 0f, 0f);
 
     public void MoveLeft()
     {
         left = true;
     }
+
     public void StopMoveLeft()
     {
         left = false;
     }
+
     public void MoveRight()
     {
         right = true;
     }
+
     public void StopMoveRight()
     {
         right = false;
     }
-    void FixedUpdate()
+
+    private void Start()
     {
-        if (left) {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        Debug.Log(T);
+        T = Obj2.transform.localEulerAngles;
+    }
+
+    private void FixedUpdate()
+    {
+        if (left)
+        {
+            Obj.transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
 
         if (right)
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Obj.transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
-        //第二种方式控制移动
+        //这一坨就是控制飞机的
         if (Input.GetKey(KeyCode.W)) //前进
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Obj.transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S)) //后退
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            Obj.transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A)) //前进
+        if (Input.GetKey(KeyCode.A)) //
         {
-            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+            Obj.transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+            Obj2.transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+            Debug.Log(T);
         }
-        if (Input.GetKey(KeyCode.D)) //后退
+        else if (!Input.GetKey(KeyCode.D) && Obj2.transform.localEulerAngles.z > 0f && 180f > Obj2.transform.localEulerAngles.z)
         {
-            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            //Obj2.transform.localEulerAngles = Vector3.Lerp(Obj2.transform.localEulerAngles, T, 0.1f);
+            Obj2.transform.Rotate(0, 0, -rightSpeed * Time.deltaTime);
+
+            //Obj2.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            Debug.Log(Obj2.transform.eulerAngles + " left");
         }
-        if (Input.GetKey(KeyCode.Q))//左平移
+
+        if (Input.GetKey(KeyCode.D)) //
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            Obj.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            Obj2.transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+            Debug.Log(Obj2.transform.eulerAngles + " right");
         }
-        if (Input.GetKey(KeyCode.E))//右平移
+        else if (!Input.GetKey(KeyCode.A) && Obj2.transform.localEulerAngles.z < 359f && 180f < Obj2.transform.localEulerAngles.z)
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            //Obj2.transform.localEulerAngles = Vector3.Lerp(Obj2.transform.localEulerAngles, T, 0.1f);
+            Obj2.transform.Rotate(0, 0, rightSpeed * Time.deltaTime);
+
+            //Obj2.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            Debug.Log(Obj2.transform.eulerAngles + " right");
         }
+
+        //到这结束
+
+        //if (Input.GetKey(KeyCode.Q))//左平移
+        //{
+        //    Obj.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.E))//右平移
+        //{
+        //    Obj.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.Q))//左桶滚
+        //{
+        //    Obj2.transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.E))//右桶滚
+        //{
+        //    Obj2.transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+        //}
     }
 }
